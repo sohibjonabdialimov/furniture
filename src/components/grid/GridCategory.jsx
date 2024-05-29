ccb5de3b68859025986429354d86495331b66cbf
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-05-30 00:17:06 +0500

    change

--- src/components/grid/GridCategory.jsx
+++ src/components/grid/GridCategory.jsx
@@ -6,6 +6,51 @@ import g4 from "../../assets/g4_stul.jpg";
 import g5 from "../../assets/g5_matras.jpg";
 import g6 from "../../assets/g6_shkaf.jpg";
 import "./grid.css";
+const data = [
+  {
+    id: 1,
+    title: "Divan va kreslolar",
+    key: "divan-va-kreslolar",
+    count: 42,
+    img: g1,
+  },
+  {
+    id: 2,
+    title: "Komadlar",
+    key: "komadlar",
+    count: 24,
+    img: g2,
+  },
+  {
+    id: 3,
+    title: "Burchakli divanlar",
+    key: "burchakli-divanlar",
+    count: 30,
+    img: g3,
+  },
+  {
+    id: 4,
+    title: "Stullar",
+    key: "stullar",
+    count: 18,
+    img: g4,
+  },
+  {
+    id: 5,
+    title: "Matraslar",
+    key: "matraslar",
+    count: 13,
+    img: g5,
+  },
+  {
+    id: 6,
+    title: "Shkaflar",
+    key: "shkaflar",
+    count: 100,
+    img: g6,
+  },
+];
+
 import { useNavigate } from "react-router-dom";
 const GridCategory = () => {
   const navigate = useNavigate();
@@ -15,78 +60,28 @@ const GridCategory = () => {
         Ommabop mebellar
       </h1>
       <div className="grid_category mt-5 sm:mt-0">
-        <div onClick={() => {navigate("/category/id")}} className="g1 grid_box cursor-pointer">
-          <img src={g1} alt="" />
-          <div className="grid_name">
-            <div>
-              <p>Divan va kreslolar</p>
-              <p>483 ta mahsulot</p>
+        {data.map((item) => {
+          return (
+            <div
+              key={item.id}
+              onClick={() => {
+                navigate(`/category/${item.key}`);
+              }}
+              className="grid_box cursor-pointer"
+            >
+              <img src={item.img} alt="" />
+              <div className="grid_name">
+                <div>
+                  <p>{item.title}</p>
+                  <p>{item.count} ta mahsulot</p>
+                </div>
+              </div>
+              <div className="grid_clip_path">
+                <p>3 454 280 UZS dan boshlab</p>
+              </div>
             </div>
-          </div>
-          <div className="grid_clip_path">
-            <p>3 454 280 UZS dan boshlab</p>
-          </div>
-        </div>
-        <div onClick={() => {navigate("/category/id")}} className="g2 grid_box">
-          <img src={g2} alt="" />
-          <div className="grid_name">
-            <div>
-              <p>Komadlar</p>
-              <p>483 ta mahsulot</p>
-            </div>
-          </div>
-          <div className="grid_clip_path">
-            <p>3 454 280 UZS dan boshlab</p>
-          </div>
-        </div>
-        <div onClick={() => {navigate("/category/id")}} className="g3 grid_box">
-          <img src={g3} alt="" />
-          <div className="grid_name">
-            <div>
-              <p>Burchakli divanlar</p>
-              <p>483 ta mahsulot</p>
-            </div>
-          </div>
-          <div className="grid_clip_path">
-            <p>3 454 280 UZS dan boshlab</p>
-          </div>
-        </div>
-        <div onClick={() => {navigate("/category/id")}} className="g4 grid_box">
-          <img src={g4} alt="" />
-          <div className="grid_name">
-            <div>
-              <p>Stullar</p>
-              <p>483 ta mahsulot</p>
-            </div>
-          </div>
-          <div className="grid_clip_path">
-            <p>3 454 280 UZS dan boshlab</p>
-          </div>
-        </div>
-        <div onClick={() => {navigate("/category/id")}} className="g5 grid_box">
-          <img src={g5} alt="" />
-          <div className="grid_name">
-            <div>
-              <p>Matraslar</p>
-              <p>483 ta mahsulot</p>
-            </div>
-          </div>
-          <div className="grid_clip_path">
-            <p>3 454 280 UZS dan boshlab</p>
-          </div>
-        </div>
-        <div onClick={() => {navigate("/category/id")}} className="g6 grid_box">
-          <img src={g6} alt="" />
-          <div className="grid_name">
-            <div>
-              <p>Shkaflar</p>
-              <p>483 ta mahsulot</p>
-            </div>
-          </div>
-          <div className="grid_clip_path">
-            <p>3 454 280 UZS dan boshlab</p>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
